b39452911c858b4c36171d01a45038c621f11400
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2022-08-04 01:19:52 +0900

    Chapter1 - 자바 프로그래밍 시작
    Hello Java이라는 문장을 화면에 출력함
    한줄 주석 연습
    여러줄 주석 연습

---
+++ JavaProgramming/src/chapter1/HelloJava.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,Java"); 
+		//한줄 주석 연습입니다.
+		
+		/* 
+		여러줄 주석 처리 하는법
+		Data 		: 2022.08.04
+		Author 		: 백종성
+		Description	: 첫 자바 프로그램입니다.
+		 */
+	}
+
+}
