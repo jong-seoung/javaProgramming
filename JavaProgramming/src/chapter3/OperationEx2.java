1d9a25168d8943c4a46d24370e958c7d291ce3b4
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2022-08-09 09:52:17 +0900

    증가, 감소 연산자
    ++ : 항의 값에 1을 더함
        예시)	 num1 = ++num;  num값이 1이 증가한후 num1에 대입
        num1 = num++;  num값을 num1에 대입한후 1만큼 증가
     -- : 항의 값에 1을 뺌
        예시)	 num1 = --num;  num값이 1이 감소 한후 num1에 대입
        num1 = num--;  num값을 num1에 대입한후 1만큼 감소

---
+++ JavaProgramming/src/chapter3/OperationEx2.java
@@ -0,0 +1,16 @@
+package chapter3;
+
+public class OperationEx2 {
+
+	public static void main(String[] args) {
+		int gameScore = 150;
+		
+		int lastScore = ++gameScore;
+		System.out.println(lastScore);
+		
+		int lastScore1 = --gameScore;
+		System.out.println(lastScore1);
+
+	}
+
+}
