ba21de6f3c6eca2ebc689a8f97556a6de2e6cfba
DESKTOP-JBJR9JL\jongseoung <jjong015189@naver.com>
2022-08-10 10:55:46 +0900

    조건 연산자를 이용하여 비교하기
    조건식 ? 결과1 : 결과2
    - 조건식이 참이면 결과1을 거짓이면 결과2가 선택됨

---
+++ JavaProgramming/src/chapter3/OperatorEx4.java
@@ -0,0 +1,17 @@
+package chapter3;
+
+public class OperatorEx4 {
+
+	public static void main(String[] args) {
+		int fatherAge = 45;
+		int motherAge = 47;
+		
+		char ch;
+		ch = (fatherAge > motherAge) ? 'T' : 'F';
+		
+		System.out.println(ch);
+
+	}
+
+}
+
